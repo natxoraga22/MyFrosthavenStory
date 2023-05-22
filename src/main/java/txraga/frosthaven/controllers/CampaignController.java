46d365203f47d86510231be6240d582a2111f9b2
Natxo Raga <nraga@csb-bi.net>
2023-05-22 17:52:36 +0200

    First draft: main page with only navbar

---
+++ src/main/java/txraga/frosthaven/controllers/CampaignController.java
@@ -0,0 +1,43 @@
+package txraga.frosthaven.controllers;
+
+import java.io.File;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.XSlf4j;
+import txraga.frosthaven.model.Scenario;
+
+
+@XSlf4j
+@Controller
+@RequestMapping("/")
+public class CampaignController {
+	
+@GetMapping("")
+	public ModelAndView campaign(Model model) {
+		log.entry();
+
+		// TEST
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			File scenarioFile = new ClassPathResource("static/json/scenarios/001.json").getFile();
+			Scenario scenario = objectMapper.readValue(scenarioFile, Scenario.class);
+			log.debug("{}", scenario);
+
+			model.addAttribute("scenario", scenario);
+		} 
+		catch (Exception e) {
+			log.catching(e);
+		}
+
+		return log.exit(new ModelAndView("campaign"));
+	}
+
+}
---
+++ src/main/java/txraga/frosthaven/model/Scenario.java
@@ -0,0 +1,22 @@
+package txraga.frosthaven.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
+public class Scenario {
+    
+    private int id;
+    private String coordinates;
+    private String name;
+    private int complexity;
+
+    private String goals;
+    private String specialRules;
+    private String introduction;
+
+}

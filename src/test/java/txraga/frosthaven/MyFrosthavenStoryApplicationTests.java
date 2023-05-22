0da0a87328145e9a2dbd9ae56b799e1c2384722d
Natxo Raga <nraga@csb-bi.net>
2023-05-22 12:58:56 +0200

    Projecte creat amb SpringBoot

---
+++ src/test/java/txraga/frosthaven/MyFrosthavenStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package txraga.frosthaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFrosthavenStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

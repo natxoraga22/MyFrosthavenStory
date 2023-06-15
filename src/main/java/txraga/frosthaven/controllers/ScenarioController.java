5296e855aa5eeb97ec211d95f3bfaac66338d63f
Natxo Raga <nraga@csb-bi.net>
2023-06-15 15:48:16 +0200

    New page to check only one scenario

--- src/main/java/txraga/frosthaven/controllers/ScenarioController.java
+++ src/main/java/txraga/frosthaven/controllers/ScenarioController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.XSlf4j;
@@ -12,14 +14,20 @@ import lombok.extern.slf4j.XSlf4j;
 
 @XSlf4j
 @Controller
-@RequestMapping("/scenario/{scenarioId}")
+@RequestMapping("/scenario")
 public class ScenarioController {
-	
-	@GetMapping("")
-	public ModelAndView campaign(Model model, @PathVariable String scenarioId) {
+
+	@GetMapping({"", "/{scenarioId}"})
+	public ModelAndView scenario(Model model, @PathVariable(required = false) String scenarioId) {
 		log.entry();
 		model.addAttribute("scenario", CampaignUtils.getScenario(scenarioId, null));
 		return log.exit(new ModelAndView("scenario"));
+	}
+
+	@PostMapping("")
+	public ModelAndView scenarioForm(Model model, @RequestParam String scenarioId) {
+		log.entry();
+		return log.exit(new ModelAndView("redirect:/scenario/" + scenarioId));
 	}
 
 }

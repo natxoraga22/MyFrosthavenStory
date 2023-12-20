96b249458f6a0dfb53ff842e9f97116301fef85e
Natxo Raga <nraga@csb-bi.net>
2023-12-20 15:25:15 +0100

    Scenario 015

--- src/main/java/txraga/mystory/frosthaven/model/ModelUtils.java
+++ src/main/java/txraga/mystory/frosthaven/model/ModelUtils.java
@@ -11,6 +11,11 @@ public final class ModelUtils {
 		String sectionIconHtml = "<img class=\"icon section\" src=\"/img/icons/section.png\"> <strong>$1</strong>";
 		String replaced = original.replaceAll(sectionIconRegex, sectionIconHtml);
 
+		// Scenario icon
+		String scenarioIconRegex = "\\[scenario_(.*?)\\]";
+		String scenarioIconHtml = "<span class=\"scenarioNumber\">($1)</span>";
+		replaced = replaced.replaceAll(scenarioIconRegex, scenarioIconHtml);
+
 		return replaced.replaceAll("\\[(.*?)\\]", "<img class=\"icon $1\" src=\"/img/icons/$1.png\">");
 	}
 

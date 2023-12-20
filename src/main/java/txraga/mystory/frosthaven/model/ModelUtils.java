0c122f39a7d3d5c3d741336d60fde7dcf18238b6
Natxo Raga <nraga@csb-bi.net>
2023-12-20 10:00:29 +0100

    Scenario 008 and event WO-10

--- src/main/java/txraga/mystory/frosthaven/model/ModelUtils.java
+++ src/main/java/txraga/mystory/frosthaven/model/ModelUtils.java
@@ -5,7 +5,13 @@ public final class ModelUtils {
 	
 	public static String replaceIcons(String original) {
 		if (original == null) return original;
-		return original.replaceAll("\\[(.*?)\\]", "<img class=\"icon $1\" src=\"/img/icons/$1.png\">");
+
+		// Section icon (special case)
+		String sectionIconRegex = "\\[section_(.*?)\\]";
+		String sectionIconHtml = "<img class=\"icon section\" src=\"/img/icons/section.png\"> <strong>$1</strong>";
+		String replaced = original.replaceAll(sectionIconRegex, sectionIconHtml);
+
+		return replaced.replaceAll("\\[(.*?)\\]", "<img class=\"icon $1\" src=\"/img/icons/$1.png\">");
 	}
 
 }

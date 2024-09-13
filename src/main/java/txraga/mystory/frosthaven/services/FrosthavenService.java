be89b1b5e55e731d9bb9178b985d7d20cea5681f
Natxo Raga <nraga@csb-bi.net>
2024-09-13 12:54:31 +0200

    Changes to layout.html and event.html in order to adapt the code to BootstrapThymeleafDialect 2.0

--- src/main/java/txraga/mystory/frosthaven/services/FrosthavenService.java
+++ src/main/java/txraga/mystory/frosthaven/services/FrosthavenService.java
@@ -12,25 +12,25 @@ import txraga.mystory.frosthaven.model.Section;
 
 public interface FrosthavenService {
 
-	/** Gets chapter "Welcome to Frosthaven" content from "welcome.txt" file */
+	/** Gets chapter "Welcome to Frosthaven" content from "welcome.txt" file. */
 	public String getWelcome();
 
-	/** Gets all characters from "characters.json" file */
+	/** Gets all characters from "characters.json" file. */
 	public Map<String,FhCharacter> findAllCharacters();
 
-	/** Gets all personal quests from "personalQuests.json" file */
+	/** Gets all personal quests from "personalQuests.json" file. */
 	public Map<String,PersonalQuest> findAllPersonalQuests(Map<String,Building> buildings);
 
-	/** Gets all events (road, outpost, etc.) from all files inside "events" folder (there is a file for each type/season) */
+	/** Gets all events (road, outpost, etc.) from all files inside "events" folder (there is a file for each type/season). */
 	public Map<String,Event> findAllEvents(Map<String,Scenario> scenarios);
 
-	/** Gets all scenarios from all files inside "scenarios" folder (there is a file for each scenario) */
+	/** Gets all scenarios from all files inside "scenarios" folder (there is a file for each scenario). */
 	public Map<String,Scenario> findAllScenarios();
 
-	/** Gets all buildings from "buildings.json" file */
+	/** Gets all buildings from "buildings.json" file. */
 	public Map<String,Building> findAllBuildings(Map<String,Scenario> scenarios);
 
-	/** Gets all sections not related to scenarios (from the Section Book) from "sections.json" file */
+	/** Gets all sections not related to scenarios (from the Section Book) from "sections.json" file. */
 	public Map<String,Section> findAllSections(Map<String,Scenario> scenarios);
 
 }

73bccf7f5999375066f0f3a64af87547a54d2cbd
Natxo Raga <nraga@csb-bi.net>
2024-09-30 12:19:35 +0200

    Last changes to Event page. Every change to the form changes the event without submiting

--- src/main/java/txraga/mystory/frosthaven/controllers/EventController.java
+++ src/main/java/txraga/mystory/frosthaven/controllers/EventController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,15 +47,6 @@ public class EventController {
 	public Event getEvent(@PathVariable String eventId) {
 		log.entry(eventId);
 		return log.exit(frosthaven.getEvent(eventId));
-	}
-
-	@PostMapping
-	public ModelAndView eventFormSubmit(Model model, @RequestParam String eventId, 
-	                                                 @RequestParam(required = false) String chosenOptions) {
-		log.entry(eventId, chosenOptions);
-		String redirectUrl = WebPage.EVENT.getUrl().replace("{eventId}", eventId);
-		if (chosenOptions != null) redirectUrl += "?chosenOptions=" + chosenOptions;
-		return log.exit(new ModelAndView("redirect:" + redirectUrl));
 	}
 	
 }
